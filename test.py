a766025d153c2b321e84ce052bb8378bb08bca40
liujian3 <liujian3@gmail.com>
2020-06-30 17:07:50 +0800

    Update test.py
    revert

--- test.py
+++ test.py
@@ -19,10 +19,10 @@ def geturl(c,s,outfile):
         tag=soup.new_tag('a', attrs={'href':'/test/'+fpth})
         tag.append('快照')
         child.append(tag)
-        fpth=outfile+str(c)+'t.txt'
+        fpth=outfile+str(c)+'t.html'
         f=open(fpth,'w')
         ssoup=BeautifulSoup(res.content,features="html.parser")
-        f.write(ssoup.text)
+        f.write('<html><head><meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body>'+ssoup.text+'</body></html>')
         f.close()
         child.a['href']=url
         tag=soup.new_tag('a', attrs={'href':'/test/'+fpth})
