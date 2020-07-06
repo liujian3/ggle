4bce0d846980d98e1caa3a11558938a6d70933f8
liujian3 <liujian3@gmail.com>
2020-07-06 12:34:22 +0800

    Update ggle.py

--- ggle.py
+++ ggle.py
@@ -25,6 +25,7 @@ def geturl(c,s,outfile):
         ssoup=BeautifulSoup(res.content,features="html.parser")
         f.write('<html><head><meta charset="utf-8"><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body>'+ssoup.text+'</body></html>')
         f.close()
+        print(fpth)
         child.a['href']=url
         tag=soup.new_tag('a', attrs={'href':'/test/'+fpth})
         tag.append('文本')
@@ -51,7 +52,7 @@ if __name__=='__main__':
     elif pn>2:
         url='http://www.google.com/search?q='+sys.argv[2]
         try:
-            url+='&source='+sys.argv[1]
+            url+='&source=lnms&tbm='+sys.argv[1]
             url+='&num='+sys.argv[3]
             url+='&start='+sys.argv[4]
             outfile=sys.argv[5]
@@ -74,3 +75,4 @@ if __name__=='__main__':
     f=open(outfile+'.html','w')
     f.write(str(soup))
     f.close()
+    print(outfile+'.html')
