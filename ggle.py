9c62f88b841dcdab155159e19aecde6c6e88b308
liujian3 <liujian3@gmail.com>
2020-07-06 12:45:30 +0800

    Update ggle.py

--- ggle.py
+++ ggle.py
@@ -50,9 +50,13 @@ if __name__=='__main__':
     if pn==2:
         url=sys.argv[1]
     elif pn>2:
-        url='http://www.google.com/search?q='+sys.argv[2]
+        source=sys.argv[1]
+        if source=='web':
+            url='http://www.google.com/search?q='
+        elif source=='news':
+            url='http://news.google.com/news?q='
+        url+=sys.argv[2]
         try:
-            url+='&source=lnms&tbm='+sys.argv[1]
             url+='&num='+sys.argv[3]
             url+='&start='+sys.argv[4]
             outfile=sys.argv[5]
